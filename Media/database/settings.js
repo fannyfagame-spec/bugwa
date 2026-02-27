5bbeb2e062801a0b060e866f5ff33f866d247199
fannyfagame-spec <fannyfagame@gmail.com>
2026-02-27 19:54:20 +0700

    Add files via upload
    Signed-off-by: fannyfagame-spec <fannyfagame@gmail.com>

---
+++ Media/database/settings.js
@@ -0,0 +1,34 @@
+module.exports = {
+  tokens: "8390230405:AAECDynhMQh0laTki183_nnE1w5I3eGKaIg", 
+  officialToken: "8390230405:AAECDynhMQh0laTki183_nnE1w5I3eGKaIg",
+  owner: "1285824577", 
+  ipvps: "127.0.0.1",
+  port: "3000"
+};
+/*
+BY : FannyFa Developer
+Base : FannyFa Developer
+V  : 1.0
+
+RULES!
+- Jangan Di Share Cuma Cuma
+- Sc Akan Dapat Update Hingga V2
+- Kalau Mau Beli Basenya 
+  Chat : 6285624179853
+  Tele : FannyFaDeveloper
+
+PENGGUNAAN :
+  - Masukan Ipvps dan ip Port
+   Contoh : ipsvps : https://fannyfa.vps.com
+             port : 3000
+  - Masukan Id Telegram Dan Token Bot Telegram
+  - Masukan Token Telegram Juga Di Bagian OfficialToken
+  - Masukan Id Kamu Sendiri Sebagai Owner
+
+Credits :
+  - FannyFa Developer 「Developer」
+  
+Tq To
+  - FannyFa Developer 「Developer」
+  - Pembeli 「Support」
+*/
